c26da2c011425ebb3413d097fd5ddd1b398e339d
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2026-05-17 21:01:32 +0300

    Update admins.routes.ts

--- backend/src/routes/admins.routes.ts
+++ backend/src/routes/admins.routes.ts
@@ -1,4 +1,3 @@
-
 import { Router } from "express";
 import { AdminsController, AuthController } from "../controllers/admins.controller";
 import { requireAuth, requireRole } from "../middleware/auth.middleware";
